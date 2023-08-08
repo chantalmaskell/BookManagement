1f761cde7dc2d6305879114d5d418d6f3b4e97c9
Chantal.Maskell <Chantal.Maskell@LPT-7FFSTV3>
2023-08-08 18:23:04 +0100

    Add project files.

---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,36 @@
+using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components.Web;
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSingleton<BlazorApp1.Database.DatabaseConnector>(_ =>
+{
+    return new BlazorApp1.Database.DatabaseConnector("Server=localhost;Port=5432;User ID=root;Password=00mB%Gv0pT4L;Database=bookapp");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();

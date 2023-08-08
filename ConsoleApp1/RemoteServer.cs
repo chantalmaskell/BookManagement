c9faf6d10aa74d12ada82fa49ba43307e84a8b3f
Chantal.Maskell <Chantal.Maskell@LPT-7FFSTV3>
2023-08-08 11:01:16 +0100

    Add project files.

---
+++ ConsoleApp1/RemoteServer.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Runtime.Remoting
+{
+    using System.Runtime.Remoting;
+    using System.Runtime.Remoting.Channels;
+    using System.Runtime.Remoting.Channels.Tcp;
+
+    TcpChannel chan = new TcpChannel(8085);
+    ChannelServices.RegisterChannel(chan);}
